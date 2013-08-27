636215d2d8d0efe32f31e4b14b0931cde2033811
Anthony DeLorenzo <aldelorenzo@ufl.edu>
2013-08-27 10:44:21 -0400

    adding function to test

--- js/index.js
+++ js/index.js
@@ -54,15 +54,11 @@ var app = {
 
             scanner.scan( function (result) { 
 
-                alert("We got a barcode\n" + 
-                "Result: " + result.text + "\n" + 
-                "Format: " + result.format + "\n" + 
-                "Cancelled: " + result.cancelled);  
+                foundLocation(result.text)
 
-               console.log("Scanner result: \n" +
-                    "text: " + result.text + "\n" +
-                    "format: " + result.format + "\n" +
-                    "cancelled: " + result.cancelled + "\n");
+                //alert("We got a barcode\n" + "Result: " + result.text + "\n" + "Format: " + result.format + "\n" + "Cancelled: " + result.cancelled);  
+
+        
                 document.getElementById("info").innerHTML = result.text;
                 console.log(result);
                 /*
@@ -80,3 +76,7 @@ var app = {
     }
 
 };
+
+foundLocation(locationID){
+    alert(locationID);
+}
