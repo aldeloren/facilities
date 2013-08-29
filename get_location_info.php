5ee6e0c3001e2ccd9c9855dfc935ac503aa5679b
Anthony DeLorenzo <aldelorenzo@ufl.edu>
2013-08-29 10:05:32 -0400

    updating styles, adding buttons, making db calls

---
+++ get_location_info.php
@@ -0,0 +1,25 @@
+<?php
+error_reporting(-1);
+$mysqli = new mysqli('localhost','facile','rB@!fadR','facile');
+
+/* check connection */
+if (mysqli_connect_errno()) {
+    printf("Connect failed: %s\n", mysqli_connect_error());
+    exit();
+}
+$loc_id = $_GET['loc_id'];
+$query = "SELECT * FROM loc_detail WHERE loc_id='$loc_id'";
+$result = $mysqli->query($query);
+$jsonData = [];
+while($row = mysqli_fetch_array($result,MYSQLI_ASSOC)){
+   array_push($jsonData,$row);
+   //$arrayItemName = $row["loc_name"];
+}
+
+//$jsonFile = "locations.json";
+//$jsonFileOpen = fopen($jsonFile, 'w');
+$jsonData = (json_encode($jsonData));
+echo $jsonData;
+//fwrite($jsonFileOpen,$jsonData);
+//fclose($jsonFileOpen);
+?>
--- js/index.js
+++ js/index.js
@@ -1,7 +1,3 @@
-function somethingScanned(locationId){
-    alert(locationId);
-}
-
 var app = {
     // Application Constructor
     initialize: function() {
@@ -38,28 +34,16 @@ var app = {
         try {
             var scanner = cordova.require("cordova/plugin/BarcodeScanner");
 
-            scanner.scan( function (result) { 
-
-
-                // alert("We got a barcode\n" + 
-                // "Result: " + result.text + "\n" + 
-                // "Format: " + result.format + "\n" + 
-                // "Cancelled: " + result.cancelled);  
-
-                somethingScanned(result.text);
-
-                //alert("We got a barcode\n" + "Result: " + result.text + "\n" + "Format: " + result.format + "\n" + "Cancelled: " + result.cancelled);  
-
-                document.getElementById("info").innerHTML = result.text;
-                console.log(result);
+            scanner.scan( function (result) {  
+                loc_id = result.text;
+                getLocationInfo(loc_id);
                 /*
                 if (args.format == "QR_CODE") {
                     window.plugins.childBrowser.showWebPage(args.text, { showLocationBar: false });
                 }
                 */
-
             }, function (error) { 
-                console.log("Scanning failed: ", error); 
+                alert("Scanning failed: try again later. error:", error); 
             } );
         } catch (ex) {
             console.log(ex.message);
